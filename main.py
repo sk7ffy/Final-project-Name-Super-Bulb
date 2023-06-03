1c7d59268f10fbde697922f25a175be16985950c
sk7ffy <115887660+sk7ffy@users.noreply.github.com>
2023-06-03 19:33:44 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -20,15 +20,15 @@ clock = time.Clock()
 game = True
 start_btn = Button(250,200,200,200,'start.png')
 exit_btn = Button(250,400,200,100,'exit.png')
-player = Player(100,200,50,50,'player.png')
-enemy = Enemy(300,410,45,50,'amogus.png')
+player= Player(100,200,50,50,'player111.png')
+enemy = Enemy(300,410,45,50,'enemy3.png')
 floor1 = Sprite(15,480,1000,200,'floor1.png')
-island4 = Sprite(200,420,170,170,'island4.png')
-island5 = Sprite(10,120,170,170,'island4.png')
-island6 = Sprite(400,120,170,170,'island4.png')
-treasure1 = Sprite(100,400,100,100,'treasure.png')
-treasure2 = Sprite(500,200,100,100,'treasure.png')
-treasure3 = Sprite(300,400,100,100,'treasure.png')
+islan = Sprite(200,227,150,50,'islan.png')
+island5 = Sprite(10,320,150,50,'islan.png')
+island6 = Sprite(500,270,150,50,'islan.png')
+treasure1 = Sprite(100,400,70,70,'treasure.png')
+treasure2 = Sprite(500,200,70,70,'treasure.png')
+treasure3 = Sprite(300,140,70,70,'treasure.png')
 
 bg = image.load('background.png')
 bg=transform.scale(bg,(900,500))
@@ -76,11 +76,11 @@ while game:
         enemy.draw(main_win)
         player.draw(main_win)
         floor1.draw(main_win)
-        island4.draw(main_win)
+        islan.draw(main_win)
         island5.draw(main_win)
         island6.draw(main_win)
-        if player.rect.colliderect(island4.rect):
-            player.rect.bottom = island4.rect.top
+        if player.rect.colliderect(islan.rect):
+            player.rect.bottom = islan.rect.top
         if player.rect.colliderect(island5.rect):
             player.rect.bottom = island5.rect.top
         if player.rect.colliderect(island6.rect):
@@ -88,8 +88,8 @@ while game:
         if player.rect.colliderect(enemy.rect):
             main_win.blit(text2,(0,220))
             mixer.music.play()
-            display.update()
-            sleep(1)
+            display.update() 
+            sleep(2)
             player.rect.x = 70
             player.rect.y = 50
         if player.rect.colliderect(treasure1.rect):
@@ -101,6 +101,14 @@ while game:
         if player.rect.colliderect(treasure3.rect):
             score = score + 1
             treasure3.rect.x = 800
+        if score == 3:
+            islan.rect.x = 100
+            islan.rect.y = 100
+            island5.rect.x = 100
+            island5.rect.y = 280
+            
+
+        
 
 
 
